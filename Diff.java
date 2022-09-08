bf8aa459c876542c84e0f14966a6e5b00970648b
Manish Kumar <manishkumar120648@gmail.com>
2022-09-08 23:10:13 +0530

    Added implementation for Diff.java

--- Diff.java
+++ Diff.java
@@ -2,7 +2,10 @@ package LearnGit;
 
 public class Diff {
     public static void main() {
-        
+        int a = 1;
+        int b = 5;
+        int diff = a-b;
+        System.out.println(diff);
     }
     
 }
